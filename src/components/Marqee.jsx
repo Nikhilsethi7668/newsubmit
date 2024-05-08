023ab6561380bd779f96b08b7726003f293bc335
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2024-05-08 18:59:33 +0530

    Update Marqee.jsx

--- src/components/Marqee.jsx
+++ src/components/Marqee.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 
 function Marqee() {
     return (
-        <div data-scroll data-scroll-speed='0.1' className="marquee h-[50vh] overflow-hidden">
-            <div className="start bg-black h-[100%] flex whitespace-nowrap leading-none">
+        <div data-scroll data-scroll-speed='0.1' className="marquee h-[50vh] overflow-hidden relative top-[10vh] ">
+            <div className="start bg-blue-300 h-[100%] flex whitespace-nowrap leading-none absolute top-[10vh]">
 
                 <motion.h1
                     initial={{ x: "0" }}
                     animate={{ x: "-100%" }}
-                    transition={{ repeat: Infinity, ease: "linear", duration: 6 }}
-                    className='text-[35vh]  text-white'>
-                    WE PROVIDE RIGHT PATH  WE PROVIDE RIGHT PATH
+                    transition={{ repeat: Infinity, ease: "linear", duration: 8 }}
+                    className='text-[35vh]  text-red-800'>
+                    JOIN US TO GET BRIGHT FUTURE || IMSEC ||
                 </motion.h1>
 
             </div>
