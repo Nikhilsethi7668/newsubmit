11cfea8c05a4665f93c127a551d447a284e99166
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2024-05-08 19:00:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 
 const Footer = () => {
+
     return (
-        <footer className="bg-black h-[100vh] flex items-center">
+        <footer data-scroll data-scroll-speed='0.01' className="bg-black h-[100vh] flex items-center">
             <div className="container mx-auto py-8">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     <div>
                         <h4 className="text-white">About Us</h4>
                         <p className="text-gray-300">IMSEC College is dedicated to providing quality education and fostering personal development among its students.</p>
                     </div>
-                    <div>
-                        <h4 className="text-white">Contact Us</h4>
-                        <p className="text-gray-300">Address: [Your Address]</p>
-                        <p className="text-gray-300">Email: info@imsec.edu</p>
-                        <p className="text-gray-300">Phone: +1234567890</p>
+                    <div className='flex flex-col gap-3'>
+                        <h4 className="text-white hover:bg-white hover:border rounded-xl hover:text-black">Contact Us</h4>
+                        <p className="text-gray-300 hover:bg-white hover:border rounded-xl hover:text-black">Address: [Your Address]</p>
+
+                        <p className="text-gray-300 hover:bg-white hover:border rounded-xl hover:text-black">Email: info@imsec.edu</p>
+                        <p className="text-gray-300 hover:bg-white hover:border rounded-xl hover:text-black">Phone: +1234567890</p>
                     </div>
                     <div>
                         <h4 className="text-white">Quick Links</h4>
                         <ul>
-                            <li><a href="http://www.imsec.ac.in/" className="text-gray-300">Home</a></li>
-                            <li><a href="http://imsec.ac.in/academics/courses" className="text-gray-300">Courses</a></li>
-                            <li><a href="http://imsec.ac.in/admissions/admission-procedure" className="text-gray-300">Admissions</a></li>
-                            <li><a href="http://imsec.ac.in/about/contact-us" className="text-gray-300">Contact</a></li>
+                            <li className='hover:bg-white hover:font-black hover:border rounded-xl hover:text-black'><a href="http://www.imsec.ac.in/" className="text-gray-300">Home</a></li>
+                            <li className='hover:bg-white hover:border hover:font-black rounded-xl hover:text-black'><a href="http://imsec.ac.in/academics/courses" className="text-gray-300">Courses</a></li>
+                            <li className='hover:bg-white hover:border rounded-xl hover:text-black'><a href="http://imsec.ac.in/admissions/admission-procedure" className="text-gray-300 hover:font-black">Admissions</a></li>
+                            <li><a href="http://imsec.ac.in/about/contact-us" className="text-gray-300 hover:bg-white hover:border rounded-xl hover:text-black hover:font-black">Contact</a></li>
                         </ul>
                     </div>
                 </div>
@@ -34,3 +36,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
