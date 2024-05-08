72bebc6b5ebe43ceaa438a4888ef49ddb2e7d15e
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2024-05-08 19:00:31 +0530

    Update First.jsx

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -5,7 +5,7 @@ import { FaArrowRight, FaRegArrowAltCircleRight } from "react-icons/fa";
 const First = () => {
 
     return (
-        <div data-scroll data-scroll-speed='-.9' className="first ">
+        <div data-scroll data-scroll-section data-scroll-speed='-.9' className="first ">
             <div className="main flex w-[100%] h-[100%] items-center">
 
                 <div className='font-bold h-[100vh] text-[18vh] sm:text-[18vh] leading-none flex flex-col justify-center ml-[5vw]'>
@@ -28,7 +28,7 @@ const First = () => {
 
 
                 </div>
-                <div className="img border rounded-lg relative top-1/2"><img src="https://lh3.googleusercontent.com/proxy/G9iwWOBBGOjcByltFWjvkr7rp2E_XHL7Mju1-L5yDzpbc3R29Qvl7HVemzhgS8ZLaxNI-Kfa82CNzu_Nvzu_H6GZAwUaHwtkO_UUhy23" alt="" /></div>
+                <div className="img border rounded-lg relative top-0"><img src="https://lh3.googleusercontent.com/proxy/G9iwWOBBGOjcByltFWjvkr7rp2E_XHL7Mju1-L5yDzpbc3R29Qvl7HVemzhgS8ZLaxNI-Kfa82CNzu_Nvzu_H6GZAwUaHwtkO_UUhy23" alt="" /></div>
             </div>
             <div className="line w-[100vw] bg-black h-[0.1vh] relative -top-[20vh]"></div>
             <div className="bottom relative -top-[18vh] flex justify-between items-center ">
@@ -38,7 +38,7 @@ const First = () => {
 
                 ))}
                 <div className="right flex items-center  ">
-                    <button className='border border-black rounded-full h-[5vh] mx-5 flex items-center justify-center p-5 hover:bg-black hover:text-white'> ForAdmission </button>
+                    <button className='border border-black rounded-full h-[5vh] mx-5 flex items-center justify-center p-5 hover:bg-black hover:text-white'> <a href="http://imsec.ac.in/admissions/admission-procedure">ForAdmission</a>  </button>
                     <FaRegArrowAltCircleRight className='h-[8vh] w-[8vh] -rotate-45 hover:bg-gray-700  border rounded-full hover:text-white ' />
                 </div>
 
